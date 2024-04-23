470cc3c8fe4379bd7c46ba8a73e92ef9f892312b
Blasnjacobo <blasnjacobo05@gmail.com>
2024-04-23 16:17:29 -0700

    categoriasHome more than 1 img

--- frontend/src/components/Home/CategoriasHome.tsx
+++ frontend/src/components/Home/CategoriasHome.tsx
@@ -1,20 +1,44 @@
-import categoriasHome from '../../assets/categoriasHome/categoriasHome.json';
+import { useState, useEffect } from 'react';
+import categoriasHomeData from '../../assets/categoriasHome/categoriasHome.json';
+
+interface Categoria {
+  id: string;
+  nombre: string;
+  descripcion: string;
+  img1: string;
+  img2: string;
+  img3: string;
+}
 
 const CategoriasHome = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex(prevIndex => (prevIndex + 1) % 3);
+    }, 10000); 
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="categoriasHome-container">
       <h1>Categorias</h1>
-      {categoriasHome.map(item => (
-          <div className="card" key={item.id}>
-            <img className="card-img-top" src={item.img2} alt={`${item.nombre} image`} />
-            <div className="card-body">
-              <h2 className="card-title">{item.nombre}</h2>
-              <p className="card-text">{item.descripcion}</p>
-            </div>
+      {categoriasHomeData.map((item: Categoria) => (
+        <div className="card" key={item.id}>
+          <img
+            className="card-img-top"
+            src={item[`img${currentImageIndex + 1}` as keyof Categoria]} // Type assertion here
+            alt={`${item.nombre} image`}
+          />
+          <div className="card-body">
+            <h2 className="card-title">{item.nombre}</h2>
+            <p className="card-text">{item.descripcion}</p>
           </div>
+        </div>
       ))}
     </div>
   );
-}
+};
 
 export default CategoriasHome;
