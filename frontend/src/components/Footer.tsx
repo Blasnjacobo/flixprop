ca736a4a6101ccad696e0df85770a9e745e051c8
Blasnjacobo <blasnjacobo05@gmail.com>
2024-04-25 15:51:38 -0700

    footer done

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <div className="footer">
         <div className="footer-container">
+            <div className="footer-main">
             <section className="footer-section">
                 <h2>Nuestra Cultura</h2>
                 <NavLink className="footer-links" to="/flixprop/colabora-con-nosotros/">Nuestra Misión</NavLink>
@@ -20,6 +21,18 @@ const Footer = () => {
                 <NavLink className="footer-links" to="/flixprop/politica-de-privacidad/">Política de privacidad</NavLink>
                 <NavLink className="footer-links" to="/flixprop/terminos-y-condiciones/">Términos y condiciones</NavLink>
                 <NavLink className="footer-links" to="/flixprop/compra-segura/">Compra Segura</NavLink>
+            </section>
+            </div>
+            <section className="newspaper-social">
+                    <p>¡Subscríbete gratis para ser el primero en enterarte de los productos más populares del momento!</p>
+                    <input className="footer-newspaper" type="email" id="email" placeholder="Correo Electrónico"/>
+                    <div className="footer-socialMedia">
+                        <i className="bi bi-twitter"></i>
+                        <i className="bi bi-facebook"></i>
+                        <i className="bi bi-instagram"></i>
+                        <i className="bi bi-tiktok"></i>
+                        <i className="bi bi-youtube"></i>
+                    </div>
             </section>
         </div>
     </div>
