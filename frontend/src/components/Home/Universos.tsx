c1436083227da288cf48b1e8cb35840804ab709a
Blasnjacobo <blasnjacobo05@gmail.com>
2024-04-26 14:58:14 -0700

    universos test 3  sliding with finger

--- frontend/src/components/Home/Universos.tsx
+++ frontend/src/components/Home/Universos.tsx
@@ -1,8 +1,9 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import universos from '../../assets/Universos/universos.json';
 
 const Universos = () => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const [touchStartX, setTouchStartX] = useState<number | null>(null);
 
   const scrollLeft = () => {
     if (containerRef.current) {
@@ -16,13 +17,34 @@ const Universos = () => {
     }
   };
 
+  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    setTouchStartX(e.touches[0].clientX);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+    if (touchStartX !== null && containerRef.current) {
+      const touchMoveX = e.touches[0].clientX;
+      const deltaX = touchStartX - touchMoveX;
+      if (deltaX > 0) {
+        containerRef.current.scrollLeft += 300;
+      } else {
+        containerRef.current.scrollLeft -= 300;
+      }
+      setTouchStartX(touchMoveX);
+    }
+  };
+
+  const handleTouchEnd = () => {
+    setTouchStartX(null);
+  };
+
   return (
     <div className='home-universo'>
       <div className='home-universo-container'>
         <section className='home-universo-title'>
           <h2>UNIVERSOS</h2>
         </section>
-        <section className='home-universo-main' ref={containerRef}>
+        <section className='home-universo-main' ref={containerRef} onTouchStart={handleTouchStart} onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
           {universos.map((universo) => (
             <div className='universo-card' key={universo.id}>
               <img src={universo.img} alt={universo.name} />
@@ -30,11 +52,11 @@ const Universos = () => {
             </div>
           ))}
         </section>
-        <div className='scroll-arrows'>
-          <button className='scroll-left' onClick={scrollLeft}><i className="bi bi-caret-left"></i></button>
-          <div>Flixprop</div>
-          <button className='scroll-right' onClick={scrollRight}><i className="bi bi-caret-right"></i></button>
-        </div>
+      </div>
+      <div className='scroll-arrows'>
+        <button className='scroll-left' onClick={scrollLeft}><i className="bi bi-caret-left"></i></button>
+        <div>Flixprop</div>
+        <button className='scroll-right' onClick={scrollRight}><i className="bi bi-caret-right"></i></button>
       </div>
     </div>
   );
