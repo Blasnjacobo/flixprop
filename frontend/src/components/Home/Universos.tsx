c415317018f7d372a83ef4a3734611743326f0e2
Blasnjacobo <blasnjacobo05@gmail.com>
2024-04-25 17:21:23 -0700

    universo

---
+++ frontend/src/components/Home/Universos.tsx
@@ -0,0 +1,43 @@
+import { useRef } from 'react';
+import universos from '../../assets/Universos/universos.json';
+
+const Universos = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const scrollLeft = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft -= 320;
+    }
+  };
+
+  const scrollRight = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft += 320;
+    }
+  };
+
+  return (
+    <div className='home-universo'>
+      <div className='home-universo-container'>
+        <section className='home-universo-title'>
+          <h2>UNIVERSOS</h2>
+        </section>
+        <section className='home-universo-main' ref={containerRef}>
+          {universos.map((universo) => (
+            <div className='universo-card' key={universo.id}>
+              <img src={universo.img} alt={universo.name} />
+              <div>{universo.name}</div>
+            </div>
+          ))}
+        </section>
+        <div className='scroll-arrows'>
+          <button className='scroll-left' onClick={scrollLeft}><i className="bi bi-caret-left"></i></button>
+          <div>Flixprop</div>
+          <button className='scroll-right' onClick={scrollRight}><i className="bi bi-caret-right"></i></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Universos;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import CategoriasHome from '../components/Home/CategoriasHome'
 import Ofrecemos from '../components/Home/Ofrecemos'
 import Slider from '../components/Home/Slider'
+import Universos from '../components/Home/Universos'
 
 const Home = () => {
   return (
     <div className='home-container'>
       <Slider />
+      <Universos />
       <CategoriasHome />
       <Ofrecemos />
     </div>
