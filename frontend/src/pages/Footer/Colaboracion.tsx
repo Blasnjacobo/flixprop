a792e47be8be61008df6ad8f36b9ce95205582d7
Blasnjacobo <blasnjacobo05@gmail.com>
2024-04-28 16:25:39 -0700

    Colabora con nosotros

--- frontend/src/pages/Footer/Colaboracion.tsx
+++ frontend/src/pages/Footer/Colaboracion.tsx
@@ -1,7 +1,11 @@
 const Colaboracion = () => {
   return (
-    <div>
-      Colaboracion
+    <div className="footerLinks-container">
+      <h1>Colabora con nosotros</h1>
+      <h3>Artistas:</h3>
+      <p>Si eres un artista y creas tu propios fanarts o productos sobre el mundo del entretenimiento y estas interesado en promocionar tu arte o productos a travéz de nuestra tienda en linea o nuestras redes sociales envianos un correo a <a className='email-flixprop' href="mailto:flixprop@gmail.com">flixprop@gmail.com</a>, donde uno de nuestros asesores se comunicara contigo para darte los pasos a seguir y comenzar a trabajar y crecer juntos.</p>
+      <h3>Creadores de contenido: </h3>
+      <p>Si eres un fanático del mundo del entretenimiento y creas contenido relacionado con el mismo te podría interesar colaborar con nosotros. También estamos buscando influencer o creadores de contenido que nos ayuden a crecer junto con ellos, si te interesa envianos un correo a <a className='email-flixprop' href="mailto:flixprop@gmail.com">flixprop@gmail.com</a>, donde uno de nuestros asesores se comunicara contigo para darte los pasos a seguir para comenzar a trabajar juntos.</p>
     </div>
   )
 }
