8c6dfbff44c87a219279361c08b7ddf915fc788d
Blasnjacobo <blasnjacobo05@gmail.com>
2024-04-28 16:29:39 -0700

    Sobre nosotros

--- frontend/src/pages/Footer/SobreNosotros.tsx
+++ frontend/src/pages/Footer/SobreNosotros.tsx
@@ -1,8 +1,13 @@
 const SobreNosotros = () => {
   return (
-    <div>
-      Sobre nosotros
-    </div>
+    <div className="footerLinks-container">
+    <h1>SOBRE NOSOTROS</h1>
+    <p>Flixprop es un sitio web donde los fans de verdad podran encontrar los artículos más originales y asombrosos de tus series y péliculas favoritas. Y para darle eso a los fans de verdad nos dimos cuenta que no basta vender nuestros propios productos ya que por ahí hay muchisimos más artículos asombrosos que los fans merecen conocer y nosotros no podemos proporcionar. Por esta razon también promocionamos productos de otras tienas que sabemos que te interesaran. </p>
+    <p>En Flixprop podras encontrar los productos más geniales de muchisimas tiendas en un mismo lugar, nosotros nos encargamos de buscar estos productos por todo el internet y seleccionar solo los mejores, más autenticos, de mejor calidad y los que sabemos que a fans de verdad les encantan.</p>
+    <p>Colaboramos con grandes empresas reconocidas como Amazon, Etsy, Hot Topic, BoxLunch, Wizarding World y muchas más.</p>
+    <p>El proceso es sencillo, entras, navegas entre nuestros diferentes universos (Stranger Things, Barbie, Disney, Friends, etc.) y seleccionas los productos que te llamen la atención y en la página del producto encontraras 2 opciones según sea el caso: Para productos afiliados a Flixprop encontraras el enlace directo a la página donde podras adquirir el producto. Para productos de Flixprop funciona como cuqluier otra tienda en linea, llenas tu información y continuas con el metodo de pago, tu compra sera procesada y tus productos llegar en tiempo y forma para que puedas disfrutarlos.</p>
+    <p>Tu compra e información se encontrarán completamente seguras en todo momento. Las entregas son totalmente seguras. Contamos con diferentes facilidades de pago y promociones.</p>
+  </div>
   )
 }
 
