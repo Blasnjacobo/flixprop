25d8a6bec6840a98c539213b74f911011ee558e5
Blasnjacobo <blasnjacobo05@gmail.com>
2024-05-15 14:17:16 -0700

    add index.js part 1

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,43 @@
-const express = require("express")
-const bodyParse = require("body-parser")
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cron = require("node-cron");
+dotenv.config();
+// const { updateDatabaseIfNeeded } = require("./config/dataUpdater");
+// const universos = require("./routes/universos");
+// const noticias = require("./routes/noticias");
+// const productos = require("./routes/productos");
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+app.use(morgan("dev"));
 
-app.use(bodyParse.urlencoded({ extended: true }))
-app.use(bodyParse.json())
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.status(200).send({ msg: "Hola Mundo!"})
-})
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
-app.post("/welcome", (req, res) => {
-    const { username } = req.body;
-    res.status(200).send({ msg: `Hola, ${username}`})
-})
+// app.use("/universos", universos);
+// app.use("/noticias", noticias);
+// app.use("/productos", productos);
 
-app.listen(PORT, () => {
-    console.log(`Tu server esta listo en el puerto ${PORT}`)
-})
+const PORT = process.env.PORT;
+mongoose
+  .connect(process.env.mongoDBURL, {})
+  .then(() => {
+    // Schedule cron job
+    cron.schedule("0 * * * *", async () => {
+      console.log("Checking for spreadsheet updates...");
+      // await updateDatabaseIfNeeded();
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server Port: ${PORT}, you are connected to database`);
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
