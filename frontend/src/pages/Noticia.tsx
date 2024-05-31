dad78c3b75f281624b4bb10326b0c67b94da78c7
Blasnjacobo <blasnjacobo05@gmail.com>
2024-05-31 16:50:31 -0700

    noticia page progress

--- frontend/src/pages/Noticia.tsx
+++ frontend/src/pages/Noticia.tsx
@@ -1,7 +1,21 @@
+import { useParams } from "react-router-dom";
+import useNoticias from '../../src/context/Noticias/useNoticias';
+import type { Noticia as NoticiaType } from '../types/Noticias';
+
 const Noticia = () => {
+  const { codigo } = useParams<{ codigo: string }>();
+  const noticias = useNoticias().noticias;
+  const noticia = noticias.find((noticia: NoticiaType) => noticia.codigo === codigo);
+
+  if (!noticia) {
+    return <div>Noticia no encontrada</div>;
+  }
+
+  console.log(noticia)
+
   return (
     <div>
-      Noticia
+      {noticia.descripcion}
     </div>
   )
 }
