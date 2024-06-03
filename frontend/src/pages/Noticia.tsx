f5bb443ba3fddf086ff099f835fa36b5531c5aa7
Blasnjacobo <blasnjacobo05@gmail.com>
2024-06-03 12:29:15 -0700

    noticia item intro

--- frontend/src/pages/Noticia.tsx
+++ frontend/src/pages/Noticia.tsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import useNoticias from '../../src/context/Noticias/useNoticias';
 import type { Noticia as NoticiaType } from '../types/Noticias';
+import '../css/NoticiasItem.css'
+import flixprop from '../assets/flixpropFondo.jpg'
 
 const Noticia = () => {
   const { codigo } = useParams<{ codigo: string }>();
@@ -14,8 +16,22 @@ const Noticia = () => {
   console.log(noticia)
 
   return (
-    <div>
-      {noticia.descripcion}
+    <div className="noticiasItem-section">
+      <div className="noticiasItem-container">
+        <section className="noticiasItem-intro">
+          <p>{noticia.titulo}</p>
+          <img src={noticia.img} alt={`Imagen de ${noticia.universo}`} />
+          <h5>{noticia.fecha}</h5>
+          <div className="noticiasItem-author">
+            <img src={flixprop} alt="" />
+            <h4>Equipo Flixprop</h4>
+          </div>
+        </section>
+        <section>
+          Hola
+        </section>
+      </div>
+      
     </div>
   )
 }
