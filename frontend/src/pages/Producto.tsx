3be0fc1f3ef8e8a09b42bf0f11e3aad813a40836
Blasnjacobo <blasnjacobo05@gmail.com>
2024-06-08 13:11:16 -0700

    ProductoPage design done without tallas and functionalities

--- frontend/src/pages/Producto.tsx
+++ frontend/src/pages/Producto.tsx
@@ -12,53 +12,51 @@ import '../css/ProductoPage.css'
 const Producto = () => {
   const { codigo } = useParams<{ codigo: string }>();
   const productos = useProducts().productos;
-   const universos = useUniversos().universos;
+  const universos = useUniversos().universos;
   const producto = productos.find((producto: ProductoType) => producto.codigo === codigo);
 
   const [selectedImage, setSelectedImage] = useState<string | undefined>(producto?.imgProducto);
   const [otrosUniversos, setOtrosUniversos] = useState<UniversoType[]>([]);
   const [otrosProductos, setOtrosProductos] = useState<ProductoType[]>([])
   const [productosRelacionados, setProductosRelacionados] = useState<ProductoType[]>([])
+  const [isMobile, setIsMobile] = useState<boolean>(window.innerWidth < 960)
+  const [sliderCurrentIndex, setSliderCurrentIndex] = useState<number>(0);
+
+  const images = [
+    producto?.imgProducto,
+    producto?.imgEscena,
+    producto?.imgModelo,
+    producto?.imgExtra
+  ].filter(Boolean); // Remove undefined values
 
   useEffect(() => {
-
-    const fetchOtrosUniversos = () => {
-      try {
-        if (producto) {
-          const filteredOtrosUniversos = universos.filter(element => element.universo !== producto.universo);
-          setOtrosUniversos(filteredOtrosUniversos);
-        }
-      } catch (error) {
-        console.error('Error:', error);
-      }
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 960);
     };
 
-    const fetchProductosRelacionados = () => {
-        try {
-            if (producto) {
-                const filteredProductosRelacionados = productos.filter(element => element.universo == producto.universo);
-                setProductosRelacionados(filteredProductosRelacionados);
-              }
-        } catch (error) {
-          console.error('Error:', error);
-        }
-    }
+    window.addEventListener('resize', handleResize);
 
-    const fetchOtrosProductos = () => {
-        try {
-            if (producto) {
-                const filteredOtrosProductos = productos.filter(element => element.universo !== producto.universo);
-                setOtrosProductos(filteredOtrosProductos);
-              }
-        } catch (error) {
-          console.error('Error:', error);
-        }
-    }
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-    fetchProductosRelacionados()
-    fetchOtrosUniversos()
-    fetchOtrosProductos()
-  }, [productos]);
+  useEffect(() => {
+    if (producto) {
+      const filteredOtrosUniversos = universos.filter(element => element.universo !== producto.universo);
+      setOtrosUniversos(filteredOtrosUniversos);
+
+      const filteredProductosRelacionados = productos.filter(element => element.universo === producto.universo);
+      setProductosRelacionados(filteredProductosRelacionados);
+
+      const filteredOtrosProductos = productos.filter(element => element.universo !== producto.universo);
+      setOtrosProductos(filteredOtrosProductos);
+    }
+  }, [producto, productos, universos]);
+
+  useEffect(() => {
+    setSelectedImage(images[sliderCurrentIndex]);
+  }, [sliderCurrentIndex, images]);
 
   if (!producto) {
     return <div>Producto no encontrado</div>;
@@ -68,69 +66,108 @@ const Producto = () => {
     setSelectedImage(image);
   };
 
-  console.log(producto);
+  const goToSliderSlide = (slideIndex: number) => {
+    setSliderCurrentIndex(slideIndex);
+  };
 
   return (
     <div className='ProductoPage-section'>
-        <div className='ProductoPage-container'>
+      <div className='ProductoPage-container'>
+        {
+          !isMobile ? (
             <section className='ProductoPage-productoInfo'>
-                <div className='ProductoPage-productoInfo-imagenes'>
-                    <div className='ProductoPage-productoInfo-imagenes-secundarios'>
-                        <img
-                        src={producto.imgProducto}
-                        alt={`imagen del producto del producto: ${producto.codigo}`}
-                        onClick={() => handleImageClick(producto.imgProducto)}
-                        />
-                        <img
-                        src={producto.imgEscena}
-                        alt={`imagen de la escena del producto: ${producto.codigo}`}
-                        onClick={() => handleImageClick(producto.imgEscena)}
-                        />
-                        <img
-                        src={producto.imgModelo}
-                        alt={`imagen del modelo en producto: ${producto.codigo}`}
-                        onClick={() => handleImageClick(producto.imgModelo)}
-                        />
-                        {
-                            (producto.imgExtra) && (
-                                <img
-                                src={producto.imgExtra}
-                                alt={`imagen secundaria del producto en producto: ${producto.codigo}`}
-                                onClick={() => handleImageClick(producto.imgExtra)}
-                            />
-                            )
-                        }
-                    </div>
-                    <div className='ProductoPage-productoInfo-imagenes-principal'>
-                        <img src={selectedImage} alt={`imagen del producto del producto: ${producto.codigo}`} />
-                    </div>
+              <div className='ProductoPage-productoInfo-imagenes'>
+                <div className='ProductoPage-productoInfo-imagenes-secundarios'>
+                  <img
+                    src={producto.imgProducto}
+                    alt={`imagen del producto del producto: ${producto.codigo}`}
+                    onClick={() => handleImageClick(producto.imgProducto)}
+                  />
+                  <img
+                    src={producto.imgEscena}
+                    alt={`imagen de la escena del producto: ${producto.codigo}`}
+                    onClick={() => handleImageClick(producto.imgEscena)}
+                  />
+                  <img
+                    src={producto.imgModelo}
+                    alt={`imagen del modelo en producto: ${producto.codigo}`}
+                    onClick={() => handleImageClick(producto.imgModelo)}
+                  />
+                  {producto.imgExtra && (
+                    <img
+                      src={producto.imgExtra}
+                      alt={`imagen secundaria del producto en producto: ${producto.codigo}`}
+                      onClick={() => handleImageClick(producto.imgExtra)}
+                    />
+                  )}
                 </div>
-                <div className='ProductoPage-productoInfo-details'>
-                    <div className='ProductoPage-productoInfo-details-basics'>
-                        <div>{producto.nombre}</div>
-                        <div>{producto.universo}</div>
-                        <div>${producto.precio}.00 MXN</div>
-                    </div>
-                    <div className='ProductoPage-productoInfo-details-cantidad'>
-                        <h2>-</h2>
-                        <h2>0</h2>
-                        <h2>+</h2>
-                    </div>
-                    <button>Agregar al carrito</button>
+                <div className='ProductoPage-productoInfo-imagenes-principal'>
+                  <img src={selectedImage} alt={`imagen del producto del producto: ${producto.codigo}`} />
                 </div>
+              </div>
+              <div className='ProductoPage-productoInfo-details'>
+                <div className='ProductoPage-productoInfo-details-basics'>
+                  <h2>{producto.nombre}</h2>
+                  <h5>{producto.universo}</h5>
+                  <h4>${producto.precio}.00 MXN</h4>
+                </div>
+                <div className='ProductoPage-productoInfo-details-cantidad'>
+                  <h2>-</h2>
+                  <h2>0</h2>
+                  <h2>+</h2>
+                </div>
+                <button>Agregar al carrito</button>
+              </div>
             </section>
-            <section className='ProductoPage-productoDescripcion'>
+          ) : (
+            <section className='ProductoPage-productoInfo-mobile'>
+              <div className='ProductoPage-productoInfo-titleUniverso-mobile'>
+                <h3>{producto.nombre}</h3>
+                <h5>{producto.universo}</h5>
+              </div>
+              <div className='ProductoPage-productoInfo-imagenes-mobile'>
+                <div className='ProductoPage-productoInfo-imagenes-imagen-mobile'>
+                  <img src={images[sliderCurrentIndex]} alt="" />
+                </div>
+                <div className="noticiasItem-dots-container">
+                  <div className='sliderItem-dots'>
+                    {images.map((_, idx) => (
+                      <div 
+                        key={idx}
+                        className="dotItem-container-item"
+                        onClick={() => goToSliderSlide(idx)}
+                      >
+                        <i className={`bi bi-circle-fill ${idx === sliderCurrentIndex ? "active" : ""}`}></i>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <h2>${producto.precio}.00 MXN</h2>
+              <div className='ProductoPage-productoInfo-cantidad-mobile'>
+                <h5>Cantidad</h5>
+                <div className='ProductoPage-productoInfo-cantidad-info-mobile'>
+                  <h2>-</h2>
+                  <h2>0</h2>
+                  <h2>+</h2>
+                </div>
+              </div>
+              <div className='ProductoPage-productoDescripcion'>
                 {producto.descripcion}
+              </div>
             </section>
-            <HomeProductos productos={productosRelacionados} text={'Productos Relacionados'} />
-            <HomeUniversos universos={otrosUniversos} text="Explora otros universos"/>
-            {otrosProductos.length > 0 && (
-            <HomeProductos productos={otrosProductos} text={'Podria interesarte'} />
-            )}
-            <section>
-                <Ofrecemos />
-            </section>
-        </div>
+            
+          )
+        }
+        <HomeProductos productos={productosRelacionados} text={'Productos Relacionados'} />
+        <HomeUniversos universos={otrosUniversos} text="Explora otros universos"/>
+        {otrosProductos.length > 0 && (
+          <HomeProductos productos={otrosProductos} text={'Podria interesarte'} />
+        )}
+        <section>
+          <Ofrecemos />
+        </section>
+      </div>
     </div>
   );
 };
