2bac64cc624ec4e2858baff177d0c87d65468a6d
Blasnjacobo <blasnjacobo05@gmail.com>
2024-06-11 18:03:18 -0700

    adding caregoria from db fixing errors

--- backend/controllers/getAllCategotias.js
+++ backend/controllers/getAllCategotias.js
@@ -2,7 +2,7 @@ const { Categoria } = require("../models/categoria.js")
 
 module.exports.getAllCategorias = async (req, res) => {
     try {
-        const noticias = await Categoria.find()
+        const categorias = await Categoria.find()
         return res.status(200).json({
             data: categorias
         })
