e8ebe30917817e60893f1e8dd00b8b213722ec55
Blasnjacobo <blasnjacobo05@gmail.com>
2024-06-22 12:48:07 -0700

    upgrae universoPage

--- frontend/src/pages/Universos.tsx
+++ frontend/src/pages/Universos.tsx
@@ -14,7 +14,6 @@ const Universos = () => {
     return (
         <div className="universosPage-section">
             <h3 className='universosPage-text'>Universos</h3>
-            <p>¡Bienvenido al apartado de Universos de Flixprop! Aquí, tu pasión por las series y películas toma vida de una manera única y especial. En Flixprop, sabemos que cada universo cinematográfico y televisivo tiene su propia magia, su propia historia y un lugar especial en el corazón de los fans. Por eso, hemos creado este espacio dedicado a los mundos que amas, para que puedas explorar y celebrar cada uno de ellos con la misma intensidad con la que los disfrutas en la pantalla.</p>
             <div className='universosPage-Cards-container'>
             {universos.map((universo) => (
               <div className='universosPage-card' key={universo.codigo} onClick={() => handleCardClick(universo)}>
